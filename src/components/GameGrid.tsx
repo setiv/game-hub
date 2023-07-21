35c61adc0492e7029c2b5006b6e282ce639e78a1
Seti Valencia <svalencia@snapchat.com>
2023-07-21 14:11:43 -0700

    error?

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ export const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  // if (error) return <Text>{error}</Text>;
 
   return (
     <SimpleGrid
